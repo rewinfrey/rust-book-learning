b546b9085b4023455ebf6fd8b97902d4aff55d5c
Rick Winfrey <rewinfrey@github.com>
2021-01-19 09:42:05 -0800

    Add slice-type section

---
+++ slice-type/target/rls/debug/deps/slice_type-c7ccbfadcfd55569.d
@@ -0,0 +1,5 @@
+/Users/rewinfrey/Documents/projects/rust/rust-book/slice-type/target/rls/debug/deps/slice_type-c7ccbfadcfd55569.rmeta: src/main.rs
+
+/Users/rewinfrey/Documents/projects/rust/rust-book/slice-type/target/rls/debug/deps/slice_type-c7ccbfadcfd55569.d: src/main.rs
+
+src/main.rs:
